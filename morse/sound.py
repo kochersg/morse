c796500c34fd9fd193917ab6fea5ae80bd777c88
Gerrit Kocherscheidt <gkj@kocoautomotive.de>
2021-08-15 08:08:50 +0200

    Sounderzeugung angefangen. Wavs können gespeichert werden

--- morse/sound.py
+++ morse/sound.py
@@ -4,24 +4,29 @@ import numpy as np
 import wave
 
 class CMySound():
-    def __init__(self, export_file:str='./static/sound/beep.wav'):
-        self.export_file=export_file
+    def __init__(self, 
+                export_file_short_beep:str='./static/sound/beeps.wav',
+                export_file_long_beep:str='./static/sound/beepl.wav'
+            ):
+        self.export_file_short_beep=export_file_short_beep
+        self.export_file_long_beep=export_file_long_beep
         self.freq_hertz=440
         self.sampling_rate_hertz=44100
-        self.duration_seconds=5
+        self.duration_short_seconds=.1
+        self.duration_long_seconds=.3
 
-    def generate_beep(self):
-        n_samples = self.sampling_rate_hertz*self.duration_seconds
+    def generate_beep(self,duration_seconds:float=1, export_file:str='./static/sound/beeps.wav'):
+        n_samples = int(self.sampling_rate_hertz*duration_seconds)
         x=np.arange(n_samples)
         y=32767.0*np.sin(2.0*np.pi*self.freq_hertz*x/self.sampling_rate_hertz)
-        w=wave.open(self.export_file,'w')
-        w.setparams((2,2,self.sampling_rate_hertz,n_samples,'NONE','not compressed'))
-
-        frames = ''.join(''.join(struct.pack('h', int(sample)) for sample in y))
-        w.writeframesraw(frames)
-
-        w.close()
+        y=np.int16(y)
+        frames = struct.pack('={}h'.format(len(y)),*y)
+        frames+= struct.pack('={}h'.format(len(y)),*y)
+        with wave.open(export_file,'w') as fh:
+            fh.setparams((2,2,self.sampling_rate_hertz,n_samples,'NONE','not compressed'))
+            fh.writeframesraw(frames)
 
 if __name__=='__main__':
-    ms=CMySound(export_file='./static/sound/beep.wav')
-    ms.generate_beep()
+    ms=CMySound()
+    ms.generate_beep(duration_seconds=ms.duration_short_seconds, export_file=ms.export_file_short_beep)
+    ms.generate_beep(duration_seconds=ms.duration_long_seconds, export_file=ms.export_file_long_beep)
